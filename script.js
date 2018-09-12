d6f443eafcd2de14e3fbddfe44db2be6a1827f2c
Ben Attwood <bendattwood@gmail.com>
2018-09-12 21:13:42 +0000

    Page displays both a local video, and a "remote" version of that video which has been passed over an RTCPeerConnection.

--- script.js
+++ script.js
@@ -1,7 +1,58 @@
 
 window.onload = async () => {
+    const video1Elem = document.getElementById("video1");
+    const video2Elem = document.getElementById("video2");
+
+    //Get webcam and mic stream
     const stream = await navigator.mediaDevices.getUserMedia({audio: true, video: true});
-    document.getElementById("selfVideo").srcObject = stream;
+
+    //Connect to html video element
+    video1Elem.srcObject = stream;
+
+    const peerCon1 = new RTCPeerConnection(null);
+    const peerCon2 = new RTCPeerConnection(null);
+
+    //This gets called when peerCon1.addLocalDescription is called.
+    peerCon1.onicecandidate = icecandidate_event => {
+        console.log("peerCon1 onicecandidate");
+        peerCon2.addIceCandidate(icecandidate_event.candidate);
+    };
+
+    //This gets called when peerCon2.addLocalDescription is called.
+    peerCon2.onicecandidate = icecandidate_event => {
+        console.log("peerCon2 onicecandidate");
+        peerCon1.addIceCandidate(icecandidate_event.candidate);
+    };
+
+    //This gets called when peerCon2.setRemoteDescription is called.
+    peerCon2.ontrack = track_event =>  {
+        console.log("peerCon2 ontrack");
+        video2Elem.srcObject = track_event.streams[0];
+    };
+
+
+    stream.getTracks().forEach(
+        track => {
+            console.log("adding track");
+            peerCon1.addTrack(track, stream);
+        }
+    );
+
+    peerCon1.createOffer().then(
+        description => {
+            peerCon1.setLocalDescription(description);
+            peerCon2.setRemoteDescription(description);
+
+            peerCon2.createAnswer().then(
+                   description => {
+                       peerCon2.setLocalDescription(description);
+                       peerCon1.setRemoteDescription(description);
+                   }
+            );
+        }
+    );
+
+
 };
 
 
